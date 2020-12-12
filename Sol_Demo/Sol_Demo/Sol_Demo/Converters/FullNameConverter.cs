6fbab9e8604a609515645ce79109f57baf80bfd8
Kishor Naik <kishor.naik011.net@outlook.com>
2020-12-12 17:48:02 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/Converters/FullNameConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Sol_Demo.Converters
+{
+    public sealed class FullNameConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{values[0]} {values[1]}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Sol_Demo/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MvvmHelpers;
+using Sol_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sol_Demo.ViewModels
+{
+    public class CustomerViewModel : BaseViewModel
+    {
+        public CustomerViewModel()
+        {
+            Customer = new CustomerModel();
+        }
+
+        private CustomerModel customer = null;
+
+        public CustomerModel Customer
+        {
+            get => customer;
+            set => base.SetProperty(ref customer, value);
+        }
+    }
+}
